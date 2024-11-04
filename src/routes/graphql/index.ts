d91d361b39d43168261d0ed384c1ea57025925cf
Skave-a <arrowtata@gmail.com>
2024-11-04 21:52:22 +0500

    feat: add loaders

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,13 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 import { graphql, parse, validate } from 'graphql';
 import depthLimit from 'graphql-depth-limit';
+import {
+  memberTypeLoader,
+  postLoader,
+  profileLoader,
+  userLoader,
+} from './dataloaders.js';
+import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,12 +23,17 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       const validationErrors = validate(schema, parse(req.body.query), [depthLimit(5)]);
       if (validationErrors.length) return { errors: validationErrors };
+
       const res = await graphql({
         schema,
         source: req.body.query,
-        contextValue: { prisma, dataloaders: new WeakMap() },
+        contextValue: {
+          prisma,
+          dataloaders: { userLoader, postLoader, profileLoader, memberTypeLoader },
+        },
         variableValues: req.body.variables,
       });
+
       return res;
     },
   });
